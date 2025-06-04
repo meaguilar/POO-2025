259f08f7364adb426c451d0ce903796845cf8ca8
Marlen <meaguilar@uca.edu.sv>
2025-06-04 17:13:00 -0600

    :memo:

--- ProyectoEjemploJavaFXMVC/scriptBD/QUERY-BD-POO.sql
+++ ProyectoEjemploJavaFXMVC/scriptBD/QUERY-BD-POO.sql
@@ -1,23 +1,14 @@
 -- Crear la base de datos 
-CREATE DATABASE IF NOT EXISTS POO;
+CREATE DATABASE POO;
 USE POO;
 
--- Usa la base de datos POO
-USE POO;
-GO
 
 -- Verifica si la tabla empleado existe, y si no, la crea
-IF OBJECT_ID('dbo.Persona', 'U') IS NULL
-BEGIN
-    CREATE TABLE empleado (
+CREATE TABLE Persona (
         id INT IDENTITY(1,1) PRIMARY KEY,
         nombre NVARCHAR(100) NOT NULL,
         aNac INT NOT NULL,
         genero NVARCHAR(20) NOT NULL
     );
-END
-GO
 
-SElECT * FROM Persona;
 
-SELECT SUSER_SNAME();
