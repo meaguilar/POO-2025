faeae2aea789d6c8df025137c1ffa26186f04d88
Marlen <meaguilar@uca.edu.sv>
2025-06-13 10:12:03 -0600

    :memo:

--- ProyectoEjemploJavaFXMVC/src/main/java/com/abc/proyectoejemplojavafxmvc/controladores/PersonaControlador.java
+++ ProyectoEjemploJavaFXMVC/src/main/java/com/abc/proyectoejemplojavafxmvc/controladores/PersonaControlador.java
@@ -1,6 +1,7 @@
 package com.abc.proyectoejemplojavafxmvc.controladores;
 
 import com.abc.proyectoejemplojavafxmvc.modelos.Persona;
+import com.abc.proyectoejemplojavafxmvc.modelos.PersonaDAO;
 import com.abc.proyectoejemplojavafxmvc.util.AlertUtil;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -13,14 +14,15 @@ import java.time.LocalDate;
 import java.util.List;
 
 
-
 public class PersonaControlador {
 
     private Persona persona;
+    //variables de instancia
     private int id;
     private String nombre;
     private String genero;
     private LocalDate fNac;
+    private PersonaDAO personaDAO;
 
 
     @FXML
@@ -28,6 +30,9 @@ public class PersonaControlador {
 
     @FXML
     private DatePicker fNacPicker;
+
+    @FXML
+    private Label datosLabel;
 
     @FXML
     private Button guardarButton;
@@ -44,6 +49,8 @@ public class PersonaControlador {
     @FXML
     private Button buscarButton;
 
+    @FXML
+    private VBox formVBox;
 
     @FXML
     private ComboBox<String> generoComboBox;
@@ -52,9 +59,18 @@ public class PersonaControlador {
     @FXML
     public void initialize() {
         persona = new Persona();
+        personaDAO = new PersonaDAO();
         generoComboBox.setItems(listaGenero);
         actButton.setDisable(true);
         eliminarButton.setDisable(true);
+
+
+        //Desactivar boton guardar
+     /*   guardarButton.disableProperty().bind(
+                nombreTextField.textProperty().isEmpty()
+                        .or(generoComboBox.getSelectionModel().selectedItemProperty().isNull())
+                        .or(fNacPicker.valueProperty().isNull())
+        );*/
     }
 
     /**
@@ -71,11 +87,13 @@ public class PersonaControlador {
             return;
         }
 
-        // se almacenan los datos
+        // se almacenan los datos en el objeto persona
         persona.setNombre(nombre);
         persona.setANac(fNac.getYear());
         persona.setGenero(genero);
 
+        //guardar los datos en la BD
+        personaDAO.insertar(persona);
 
         //se genera el alert de confirmación
         AlertUtil.mostrarConfirmacion("Confirmación", "Datos guardados exitosamente");
@@ -88,10 +106,41 @@ public class PersonaControlador {
     }
 
     /**
-     * Metodo buscar por id en la BD
+     * Buscar por id en la BD
      */
+
     @FXML
     private void buscar() {
+        TextInputDialog dialog = new TextInputDialog();
+        dialog.setTitle("Buscar Persona");
+        dialog.setHeaderText("Buscar Persona");
+        dialog.setContentText("Buscar Persona, ingresa el nombre");
+        dialog.showAndWait().ifPresent(input -> {
+            id = Integer.parseInt(dialog.getResult());
+            Persona personaEncontrada = personaDAO.buscarPersonaPorId(id);
+
+            if (personaEncontrada != null) {
+                nombreTextField.setText(personaEncontrada.getNombre());
+                generoComboBox.getSelectionModel().select(personaEncontrada.getGenero());
+                int aNac = personaEncontrada.getANac();
+                LocalDate fechaNacimiento = LocalDate.of(aNac, 1, 1);
+                fNacPicker.setValue(fechaNacimiento);
+                this.persona = personaEncontrada;
+                this.nombre = personaEncontrada.getNombre();
+                this.genero = personaEncontrada.getGenero();
+                this.fNac = LocalDate.of(personaEncontrada.getANac(), 1, 1);
+
+                // Deshabilitar formulario
+                formVBox.setDisable(true);
+                // Cambiar texto del botón a "Editar"
+                actButton.setText("Editar");
+                eliminarButton.setDisable(false);
+                actButton.setDisable(false);
+                guardarButton.setDisable(true);
+            } else {
+                AlertUtil.mostrarError("Error", "No existe una persona con ese id");
+            }
+        });
 
     }
 
@@ -100,7 +149,43 @@ public class PersonaControlador {
      */
     @FXML
     private void actualizar() {
+        String nombreBoton = actButton.getText();
+        if (nombreBoton.equals("Editar")) {
+            // Cambiar a modo edición
+            formVBox.setDisable(false);
+            actButton.setText("Actualizar");
 
+        } else if (nombreBoton.equals("Actualizar")) {
+
+            // validar nulos y fecha
+            if (!validarNulos() || !validarFecha()) {
+                return;
+            }
+            try {
+                // establecer en persona los valores de los componentes
+                persona.setNombre(nombreTextField.getText());
+                persona.setANac(fNacPicker.getValue().getYear());
+                persona.setGenero(generoComboBox.getSelectionModel().getSelectedItem());
+
+                // llamar al metodo actualizar de DAO
+                personaDAO.actualizar(persona, id);
+
+                // mostrar alerta de confirmación
+                AlertUtil.mostrarConfirmacion("Registro actualizado", "Datos actualizados exitosamente");
+
+                //Deshabilitar el formulario
+                formVBox.setDisable(true);
+
+                // cambiar el texto del boton actualizar a editar
+                actButton.setText("Editar");
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                // mostrar alerta de error
+                AlertUtil.mostrarError("Error", "Error al actualizar el registro de la persona");
+            }
+
+        }
     }
 
     /**
@@ -108,6 +193,23 @@ public class PersonaControlador {
      */
     @FXML
     private void eliminar() {
+        //Crear un componente Dialog con 2 botones ok, cancel
+        Dialog dialog = new Dialog();
+        dialog.setTitle("Eliminar registro de persona");
+        dialog.setContentText("¿Deseas eliminar el registro de persona?");
+        dialog.getDialogPane().getButtonTypes().setAll(ButtonType.CANCEL, ButtonType.OK);
+        //dialog.showAndWait();
+        if (dialog.showAndWait().get() == ButtonType.OK) {
+            // llamar al metodo eliminar de DAO
+            personaDAO.eliminar(id);
+            // limpiar el formulario
+            limpiar();
+            // habilitar el formulario y boton guardar
+            formVBox.setDisable(false);
+            guardarButton.setDisable(false);
+            // alerta de confirmacion
+            AlertUtil.mostrarConfirmacion("Registro eliminado", "Datos eliminados exitosamente");
+        }
 
     }
 
@@ -117,10 +219,32 @@ public class PersonaControlador {
     @FXML
     private void mostrar() {
 
-        Dialog dialog = new Dialog();
-        dialog.setContentText(persona.toString());
-        dialog.getDialogPane().getButtonTypes().setAll(ButtonType.OK);
+        //Obtener de la BD
+        List<Persona> registrosPersonas = personaDAO.listarPersonas();
+        //Validar datos en la BD
+        if (registrosPersonas.isEmpty()) {
+            AlertUtil.mostrarAdvertencia("Registro personas", "No hay registros de personas");
+            return;
+        }
+        ObservableList<Persona> observableListPersonas = FXCollections.observableArrayList(registrosPersonas);
+        //Creando componente listView
+        ListView<Persona> listView = new ListView<>(observableListPersonas);
+        listView.setPrefSize(550, 250);
+
+        // Crear el diálogo
+        Dialog<Persona> dialog = new Dialog<>();
+        dialog.setTitle("Registro de personas");
+        dialog.setHeaderText("Detalle de las personas registradas");
+
+        VBox contenedor = new VBox(listView);
+
+
+        dialog.getDialogPane().setContent(contenedor);
+        dialog.getDialogPane().getButtonTypes().addAll(ButtonType.OK);
+
         dialog.showAndWait();
+
+
         //datosLabel.setText(persona.getNombre() + " - " + persona.getGenero() + " - " + persona.calcularEdad() + "años");
     }
 
